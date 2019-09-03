05bc00d929dd858cd335c69dd30728eb145b0610
Nick Braithwaite <nick@swellfoundry.com>
2019-09-03 06:15:17 +0100

    Added in seeder to populate products table. Setup api end points, using built in methods from the Product class

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    //if the table has a different name to products we can use this property
+    //protected $table = 'custom_products';
+
+    protected $fillable = ['title', 'description', 'price', 'availability'];
 }
--- routes/channels.php
+++ routes/channels.php
@@ -14,3 +14,38 @@
 Broadcast::channel('App.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+
+/**
+** Basic Routes for a RESTful service:
+**
+** Route::get($uri, $callback);
+** Route::post($uri, $callback);
+** Route::put($uri, $callback);
+** Route::delete($uri, $callback);
+**
+**/
+ 
+Route::get('products', function () {
+    return response(['Product 1', 'Product 2', 'Product 3'],200);
+});
+ 
+Route::get('products/{product}', function ($productId) {
+    return response()->json(['productId' => "{$productId}"], 200);
+});
+  
+ 
+Route::post('products', function() {
+    return  response()->json([
+            'message' => 'Create success'
+        ], 201);
+});
+ 
+Route::put('products/{product}', function() {
+    return  response()->json([
+            'message' => 'Update success'
+        ], 200);
+});
+ 
+Route::delete('products/{product}',function() {
+    return  response()->json(null, 204);
+});
